3a8129dbc9c0bc183c576ea803321c14821a6af3
Okan Çelebi <okancelebi06@gmail.com>
2023-01-30 21:25:26 +0300

    Add files via upload

---
+++ csharp çarpma bölme operatörü/csharp çarpma bölme operatörü/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_çarpma_bölme_operatörü
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Çarpma operatörü : *
+            //Bölme operatörü : /
+            int sayi1 = 7;
+            int sayi2 = 11;
+            int sayi3 = 100;
+            int sayi4 = 10;
+            int sonuc2 = sayi3 / sayi4;
+            int sonuc = sayi1 * sayi2;
+            Console.WriteLine(sonuc);
+            Console.WriteLine(sonuc2);
+            Console.ReadLine();
+        }
+    }
+}
